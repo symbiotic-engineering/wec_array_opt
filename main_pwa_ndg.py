e479b66daf893373fdaf63816c74c61f062c7d26
degoeden <degoeden8@gmail.com>
2023-06-23 12:53:40 -0400

    first step of pwa, vector phi_star calculated, not updated phi matrix

--- main_pwa_ndg.py
+++ main_pwa_ndg.py
@@ -1,13 +1,13 @@
 # PWA - NDG's attempt
 import capytaine as capy
 import numpy as np
-from capytaine.bem.airy_waves import airy_waves_potential
 # Define Array and Waves
 wecx = [0, 50, 100]
 wecy = [0, 0, 0]
 r = 1
 omega = 1
 beta = 0
+g = 9.81
 
 # Get the Bodies
 def get_body(r,x,y):
@@ -31,8 +31,9 @@ def get_neighbors(bodies):
                 neighbors[body].append(neighbor)
     return neighbors
 neighbors = get_neighbors(bodies)
+print("Initialization completed. On to BEM...")
 
-# BEM Potentials
+# Step 1: BEM Potentials
 def bem_potentials(bodies,neighbors):
     solver = solver = capy.BEMSolver()
     diff_problem = {body:capy.DiffractionProblem(body=body,omega=omega,wave_direction=beta) for body in bodies}
@@ -43,6 +44,24 @@ def bem_potentials(bodies,neighbors):
     return phi
 
 phi = bem_potentials(bodies,neighbors)
-print(phi)
+print("BEM completed. On to PWA...")
 
-# Step 1: interactions
+# Step 2: PWA
+def calc_phi_star(bodies,neighbors,phi):
+    def pwa_interaction(body_i,body_j,phi_ij):
+        k = omega**2/g
+        x_i = body_i.home[0]
+        y_i = body_i.home[0]
+        x_j = body_j.home[0]
+        y_j = body_j.home[1]
+        theta = np.arctan((y_j-y_i)/(x_j-x_i))
+        phi_multiplier = np.exp(1j*k*((x_i-x_j)*np.cos(theta) + (y_i-y_j)*np.sin(theta))) 
+        phi_term = phi_ij*phi_multiplier
+        return phi_term
+
+    phi_star = {body:sum(pwa_interaction(neighbor,body,phi[neighbor][body]) for neighbor in neighbors[body]) for body in bodies}
+    return phi_star
+
+phi_star = calc_phi_star(bodies,neighbors,phi)
+print(phi_star)
+
