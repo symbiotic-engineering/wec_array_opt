6cb3b456e9704d3cf44ddda6eaa30d859f323da7
degoeden <degoeden8@gmail.com>
2023-06-23 12:24:17 -0400

    bem pots are done

--- main_pwa_ndg.py
+++ main_pwa_ndg.py
@@ -2,7 +2,6 @@
 import capytaine as capy
 import numpy as np
 from capytaine.bem.airy_waves import airy_waves_potential
-
 # Define Array and Waves
 wecx = [0, 50, 100]
 wecy = [0, 0, 0]
@@ -29,45 +28,21 @@ def get_neighbors(bodies):
     for body in bodies:
         for neighbor in bodies:
             if not body == neighbor:
-                neighbors[body].append(neighbor.home)
+                neighbors[body].append(neighbor)
     return neighbors
 neighbors = get_neighbors(bodies)
-print(neighbors)
 
-# BEM time - looking at each wec individually
-def indv_phi(bodies):
-    solver = capy.BEMSolver()
-    diff_problems = {body:capy.DiffractionProblem(body=body,omega=omega,wave_direction=beta) for body in bodies}
-    rad_problems  = {body:capy.RadiationProblem(body=body,omega=omega) for body in bodies}
-    diff_results  = {body:solver.solve(diff_problems[body]) for body in bodies}
-    rad_results   = {body:solver.solve(rad_problems[body]) for body in bodies}
-    phis = {body:airy_waves_potential(body.home,diff_problems[body]) for body in bodies}
-    return phis
-phis = indv_phi(bodies)
-print("================Indvidual Phis===================")
-print(phis)
-
-# BEM - off diagonals, not helpful
-def neighbor_phi(bodies,neighbors):
-    solver = capy.BEMSolver()
-    diff_problems = {body:capy.DiffractionProblem(body=body,omega=omega,wave_direction=beta) for body in bodies}
-    rad_problems  = {body:capy.RadiationProblem(body=body,omega=omega) for body in bodies}
-    diff_results  = {body:solver.solve(diff_problems[body]) for body in bodies}
-    rad_results   = {body:solver.solve(rad_problems[body]) for body in bodies}
-    phis = {body:airy_waves_potential(np.array(neighbors[body]),diff_problems[body]) for body in bodies}
-    return phis
-phis = neighbor_phi(bodies,neighbors)
-print("================Neighbor Phis===================")
-print(phis)
-
-def phi_incident(body):
-    g = 9.81
-    k = omega**2/g
-    phi = g/omega*np.exp(k*(body.home[0]*np.cos(beta) + body.home[1]*np.sin(beta))*1j)
+# BEM Potentials
+def bem_potentials(bodies,neighbors):
+    solver = solver = capy.BEMSolver()
+    diff_problem = {body:capy.DiffractionProblem(body=body,omega=omega,wave_direction=beta) for body in bodies}
+    diff_result = {body:solver.solve(diff_problem[body]) for body in bodies}
+    phi = {effector:
+           {effected:solver.compute_potential(effected.home,diff_result[effector])for effected in bodies} 
+          for effector in bodies}
     return phi
-phis = {body:phi_incident(body) for body in bodies}
-print("================Nate's Phis===================")
-print(phis)
-print("My phis are just incident, but if you multiply them by -j, you get the BEM results... hmmm")
+
+phi = bem_potentials(bodies,neighbors)
+print(phi)
 
 # Step 1: interactions
