5cd71db6839519335f4c505c6845c08f8301010c
degoeden <degoeden8@gmail.com>
2023-07-17 13:53:01 -0400

    convergence, woohoo

--- dynamics.py
+++ dynamics.py
@@ -6,7 +6,7 @@ def solve(initial_hydro,a,omega,bodies):
     C ={body:initial_hydro[body]['C'] for body in bodies}
     F ={body:initial_hydro[body]['F'] for body in bodies}
     M ={body:initial_hydro[body]['M'] for body in bodies}
-    tf = {body:(-(omega**2)*(A[body]+M[body]) + 1j*omega*B[body] + C[body])**-1 for body in bodies}
+    tf = {body:(-(omega**2)*(A[body]+M[body]) - 1j*omega*B[body] + C[body])**-1 for body in bodies}
     forces = {body1:{body2: a[body1][body2]*F[body1][body2] for body2 in bodies}for body1 in bodies}
     force_sum = {body:sum(forces[body].values()) for body in bodies}
     
--- main_take2.py
+++ main_take2.py
@@ -2,6 +2,8 @@ import wec_array_initialization as array_init
 import bem_interface as bem
 import dynamics as dyn
 import step4_interface as step4
+import numpy as np
+import matplotlib.pyplot as plt
 '''Please note that I changed how I index my 2d dictionaries for ease of coding in some parts.
 In my first attempt I did variable[effecting body][effected body] for all my interaction terms.
 Now I do variable[effected body][effecting body] because it lets me easily sum by doing
@@ -9,14 +11,24 @@ sum(variable[body].value) and it returns the total of what is effecting it inste
 of how it is effecting others. The variables this impacts are only "phi" and "a".'''
 
 # Define Array
-wecx = [0,100]
-wecy = [0,0]
+space = 100
+nx = 10
+ny = 10
+xrange = space*(nx-1)
+yrange = space*(ny-1)
+xs = np.linspace(0,xrange,nx)
+ys = np.linspace(0,yrange,ny)
+X, Y = np.meshgrid(xs, ys)
+wecx = X.flatten()
+wecy = Y.flatten()
+plt.plot(wecx,wecy,linestyle = 'none',marker = 'o')
+plt.show()
 r = 10
 T = 5
 
 # Waves
 Amp = 1
-omega = 1
+omega = 0.5
 beta = 0
 
 # Array Initialization
@@ -37,13 +49,20 @@ Xi = dyn.solve(initial_hydro,a,omega,bodies)
 
 # Step 4: Wierd PWA equations
 phi = step4.calc_phi(bodies,neighbors,Xi,initial_hydro,a,omega)
-print("=================================================================================")
-print(Xi)
 a = step4.new_a_matrix(bodies,neighbors,phi,omega,a)
 
 # Step 5: Loop
-'''
-for ii in range(2*N):
+converged = False
+while not converged:
+    a_old = a
     Xi = dyn.solve(initial_hydro,a,omega,bodies)
     phi = step4.calc_phi(bodies,neighbors,Xi,initial_hydro,a,omega)
-    a = step4.new_a_matrix(bodies,neighbors,phi,omega,a)'''
+    a = step4.new_a_matrix(bodies,neighbors,phi,omega,a)
+    a_diff =[]
+    for body1 in bodies:
+        for body2 in bodies:
+            a_diff.append(np.abs(a[body1][body2] - a_old[body1][body2]))
+    if max(a_diff) <= 0.003:
+        converged = True
+    
+print(Xi)
