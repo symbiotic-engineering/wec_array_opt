88e0a52b7c9b1582c57443aabaae9325bf9c3aeb
degoeden <degoeden8@gmail.com>
2023-07-18 16:44:12 -0400

    MOOOOOOOOOOO!

---
+++ main_soo_opt.py
@@ -0,0 +1,14 @@
+import optimization_interfaces.heuristic_interface as opt
+import numpy as numps
+# Define Parameters
+N = 3
+omega = 0.5
+beta = 0
+A = 1
+p = numps.array([omega,A,beta,N])
+# p = [Wave Frequency, Wave Amplitude, density of WEC material, number of WECs]
+
+# Limits on Design variables
+limits = {'r':[2,20], 'L':[2,20], 'x':[-1000,1000], 'y':[-1000,1000], 'd':[0,7]}
+
+opt.GA(p,limits)
