29cb569fa5fa24b6c8f46c896b0179f4c874373d
degoeden <degoeden8@gmail.com>
2023-07-20 08:46:28 -0400

    corrected parameter comment

--- main_soo_opt.py
+++ main_soo_opt.py
@@ -6,7 +6,7 @@ omega = 0.5
 beta = 0
 A = 1
 p = numps.array([omega,A,beta,N])
-# p = [Wave Frequency, Wave Amplitude, density of WEC material, number of WECs]
+# p = [Wave Frequency, Wave Amplitude, wave direction, number of WECs, free surface reach, grid point rates in free surface]
 
 # Limits on Design variables
 limits = {'r':[2,20], 'L':[2,20], 'x':[-1000,1000], 'y':[-1000,1000], 'd':[0,7]}
