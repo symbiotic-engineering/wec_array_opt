ab18bac2d52a62b0b83667c42b40b4c839a8130e
degoeden <degoeden8@gmail.com>
2023-07-21 14:28:50 -0400

    moo!

--- main_moo_opt.py
+++ main_moo_opt.py
@@ -1,21 +1,21 @@
-import optimization_interfaces.single_objective_opt as opt
-import numpy as numps
+import optimization_interfaces.multi_objective_opt as opt
+import numpy as np
 # Define Parameters
 N = 3
-omega = 1
+omega = 0.5
 beta = 0
 A = 1
-p = numps.array([omega,A,beta,N,0])
+p = np.array([omega,A,beta,N,0])
 # p = [Wave Frequency, Wave Amplitude, wave direction, number of WECs, display time stamps?]
 
 # Limits on Design variables
 limits = {'r':[2,20], 'L':[2,20], 'x':[-1000,1000], 'y':[-1000,1000], 'd':[0,7]}
 
 # Opt paramaters
-p_size = 10
-gens = 5
-n_offspring = 5
+p_size = 100
+gens = 50
+n_offspring = 50
 
-X,F,H = opt.P_GA(p,limits,p_size,gens,n_offspring)
+X,F,H = opt.MOCHA(p,limits,p_size,gens,n_offspring)
 print(X)
 print(F)
--- modules/distances.py
+++ modules/distances.py
@@ -11,3 +11,15 @@ def min_d(x,p):
             d.append(((wecx[i]-wecx[j])**2 + (wecy[i]-wecy[j])**2)**(1/2))      # Compute the distance
     mind = min(d)               # What is the shortest distance?
     return (mind)
+
+def max_d(x,p):
+    nwec = int(p[3])                 # Number of WECs
+    if nwec == 1:
+        return 0
+    wec_radius, wec_length, wecx, wecy, damp = model.unpack_x(x,nwec)
+    d = []
+    for i in range(nwec):
+        for j in range(i+1,nwec):
+            d.append(((wecx[i]-wecx[j])**2 + (wecy[i]-wecy[j])**2)**(1/2))      # Compute the distance
+    maxd = max(d)               # What is the longest distance?
+    return (maxd)
