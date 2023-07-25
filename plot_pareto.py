c35d4c3304af6f823f468e8b74ac0f4912064a44
KapilKhanal <kkhanal16@winona.edu>
2023-07-25 10:32:09 -0400

    Pareto curve remade

--- plot_pareto.py
+++ plot_pareto.py
@@ -1,28 +1,50 @@
 import matplotlib.pyplot as plt
-import csv
 
-f1,f2 = [],[]
+
+
+import csv
+import numpy as np
+import matplotlib.colors as mcolors
+from scipy.interpolate import make_interp_spline
+
+
+
+f1, f2 = [], []
 with open('./paretos/domF_0.5_1_0_10__500_400_100.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         f1.append(float(row[0]))
         f2.append(float(row[1]))
-        print(', '.join(row))
+
 utop1 = min(f1)
 utop2 = min(f2)
 nadi1 = max(f1)
 nadi2 = max(f2)
-plt.rcParams.update({'font.size': 12})
-fig = plt.figure(1,facecolor='none')
+
+# Create a colormap using colorblind-friendly colors
+cmap = plt.get_cmap('tab20', len(f1))
+
+plt.rcParams.update({'font.size': 16})  # Increase font size
+
+fig = plt.figure(1, facecolor='none')
 ax = plt.axes()
 ax.set_facecolor('none')
-plt.plot(f1,f2,marker='o',color='#72d9f0',fillstyle = 'none',label = 'Pareto Front',markersize=5,linewidth=2)
-plt.plot(utop1,utop2,marker='*',color='#a5cf27',label = 'Utopia Point',markersize=10,linewidth=2)
-plt.text(utop1+0.001,utop2+0.1,'Utopia',color='#a5cf27')
-plt.plot(nadi1,nadi2,marker = 'x',color = '#ff5858',label='Nadir Point',markersize=10,linewidth=2)
-plt.text(nadi1-0.015,nadi2-4,'Nadir',color = '#ff5858')
-plt.legend
+
+# Increase marker size and linewidth for better visibility
+plt.scatter(f1, f2, marker='o', c='tab:blue', label='Pareto Front', s=100, edgecolors='k', zorder=2)
+plt.scatter(utop1, utop2, marker='*', color='tab:green', label='Utopia Point', s=200, edgecolors='k', zorder=3)
+plt.text(utop1, utop2, 'Utopia', color='tab:green', fontsize=16)
+plt.scatter(nadi1, nadi2, marker='x', color='tab:red', label='Nadir Point', s=200, linewidth=2, zorder=3)
+plt.text(nadi1 - 0.015, nadi2 - 4, 'Nadir', color='tab:red', fontsize=16)
+
+# Interpolate a curve between the points
+
+plt.plot(f1, f2, color='tab:blue', label='Interpolated Curve', linewidth=2, zorder=1)
+
+plt.legend()
 plt.xlabel('LCOE [$/kWh]')
 plt.ylabel('Maximum Array Dimension [m]')
+plt.title("")
 
 plt.show()
+
