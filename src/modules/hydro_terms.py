1d7793fbaf9b686a863cdcb40180f52d82dbfedb
degoeden <degoeden8@gmail.com>
2024-02-21 08:56:11 -0500

    added Mathieu's fix that allows us to still use Kapil's fancy engine

--- src/modules/hydro_terms.py
+++ src/modules/hydro_terms.py
@@ -12,9 +12,7 @@ def run(bodies,beta,omega,time_data):
     # omega     ->  wave frequency
     # time_data ->  switch for outputting time data
     start_time = time.time()
-    wec_array = bodies[0]           # build the array by putting all the bodies together
-    for ii in range(len(bodies)-1):
-        wec_array+=bodies[ii+1]
+    wec_array = capy.FloatingBody.join_bodies(*bodies)  # join bodies to build array using advice from Mathieu, capy issue #457
     end_time = time.time()
     if time_data == 1:  # prints time info if switched on
         print(f'Array set up time: {end_time-start_time}')
@@ -30,8 +28,8 @@ def run(bodies,beta,omega,time_data):
 
     # Solve radiation problems, and diffraction problem
     start_time = time.time()
-    #engine = capy.HierarchicalToeplitzMatrixEngine(ACA_distance = 30,ACA_tol = 1e-1,matrix_cache_size=2) #at least three radius - ???
-    engine = capy.BasicMatrixEngine()
+    engine = capy.HierarchicalToeplitzMatrixEngine(ACA_distance = 30,ACA_tol = 1e-1,matrix_cache_size=2) #at least three radius - ???
+    #engine = capy.BasicMatrixEngine()
     solver = capy.BEMSolver(engine = engine)    # creates the solver using the defined engine
     
     # select DOFs
