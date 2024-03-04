b2b45c038d42775c7ceb2260fb9a2aa458a9bf36
KapilKhanal <kkhanal16@winona.edu>
2024-03-04 12:56:27 -0500

    demo

--- src/post_pro/sensitivity.py
+++ src/post_pro/sensitivity.py
@@ -67,6 +67,7 @@ for i, X in enumerate(param_values):
 Si = sobol.analyze(parameter_problem, Y,calc_second_order=True, num_resamples=100, conf_level=0.95, print_to_console=False)
 
 #first order sobol indices
+#
 total_Si, first_Si, second_Si = Si.to_df()
 
 Si.to_df().to_csv("data/sensitivities.csv")
