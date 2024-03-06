b54c62c5334187c9042e2c4efefcad87e0fadf36
KapilKhanal <kkhanal16@winona.edu>
2024-03-06 16:09:04 -0500

    "convergence code refined"

--- src/post_pro/mesh_convergence.py
+++ src/post_pro/mesh_convergence.py
@@ -8,83 +8,80 @@ random.seed(0)
 parent_folder = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 sys.path.insert(0,parent_folder)
 
-import modules.model_nWECs as model
+import modules.hydro_terms as hydro
 import modules.distances as dis
 
 def body_resolution(r,L,nr,ntheta,nx):
     #generate cylinder for 0,0
     mesh = capy.meshes.predefined.mesh_vertical_cylinder(radius=r,center=(0,0,0), length=L,resolution = (nr,ntheta,nx))
-    body = capy.FloatingBody(mesh)
+    body = capy.FloatingBody(mesh,dofs = capy.rigid_body_dofs(rotation_center=[0,0,0]))
     body.add_translation_dof(name='Heave')
     body.keep_immersed_part()
     body = body.immersed_part()
-    body.name = f"{radius}_{length_ratio}"
+    body.center_of_mass = (0, 0, 0)
+    body.radius = r
     return body
 
-nr = [8,10,15,20] #(nr) number of panels along a radius on the extremities of the cylinder
-ntheta = [10,20,24,30] #(ntheta) number of panels along a circular slice of the cylinder
-nx = [5,10,15,25] #number of circular slices(nx) ,
+
+def get_hydro(body):
+   
+    C = body.compute_hydrostatics()              # solves hydrostatics problem (no waves)
+    # hydrostatic stiffness
+    M = np.array(C["inertia_matrix"])     # mass
+    engine = capy.BasicMatrixEngine() 
+    solver = capy.BEMSolver(engine = engine)    # creates the solver using the defined engine
+          
+    # create problems and solve
+    rad_prob = [capy.RadiationProblem(body=body,omega=1.02,radiating_dof='Heave') for body in [body]]  # radiation
+    rad_result = solver.solve_all(rad_prob,keep_details=(True))
+    diff_prob = capy.DiffractionProblem(body=body, omega=1.02)                       # diffraction
+    diff_result = solver.solve(diff_prob,keep_details=(True))
+    
+    # Get the important stuff
+    dataset = capy.assemble_dataset(rad_result + [diff_result])
+   
+    A = dataset['added_mass'].sel(radiating_dof = 'Heave', influenced_dof = 'Heave') 
+   
+    B = dataset['radiation_damping'].sel(radiating_dof = 'Heave',influenced_dof = 'Heave')
+    return A[0],B[0]
 
 
 #Latin hypercube sampling to get representative design in the design space.
 radius,length_ratio = [1,10], [1,3]
 designs = np.array([radius, length_ratio])
 sampling = LHS(xlimits=designs)
-num = 10
+num = 100
 inputs_dim = sampling(num)
-print(f"inputs_designs are {inputs_dim}")
+
 df = pd.DataFrame(inputs_dim)
 
+nr = [8,20] #(nr) number of panels along a radius on the extremities of the cylinder
+ntheta = [10,30] #(ntheta) number of panels along a circular slice of the cylinder
+nx = [5,25] #number of circular slices(nx) ,
+
+#Latin hypercube sampling to get all representative panels in the design space.
 panels = np.array([nr,nx,ntheta])
-#Latin hypercube sampling to get representative design in the design space.
 sampling = LHS(xlimits=panels)
-num = 10
+num = 50
 inputs_panels= sampling(num)
-print("panels")
+
 print(inputs_panels)
 
-df = pd.DataFrame(inputs_dim)
-
 # model.run after pack, unpack stuff...
-
-#@NATE add this
 omega = 1.02
-iteration = 0
-print(inputs_dim)
-design_Mass = {}
+
 def by_design(inputs,panels):
-	name = f"design_{panels[0]}_{panels[1]}_{panels[2]}_{inputs[0]},{inputs[1]}"
-	bodies = [body_resolution(inputs[0],inputs[1],int(panels[0]),int(panels[1]),int(panels[2]))]
-	nb_faces = {body.name:body.mesh.nb_faces for body in bodies}
-	#ds1 = [solve_hydrodynamics(body,0.3) for body in bodies]
-	LCOE = []
-	omega_range = [omega]
-	body_name = []
-	df_list= []
-	iteration = 0
-	with ProcessPoolExecutor(20) as exe:
-	# perform calculations
-		print("inside ProcessPoolExecutor")
-		for lcoe,distance in exe.map(model.run, bodies):
-			LCOE.append(lcoe)
-			body_name.append(bodies.name)
-			pairs = [(face,lcoe) for face,lcoe,bn in zip(nb_faces.values(),lcoe,body_name)]
-			pairs = sorted(
-			   pairs, 
-			    key=lambda x: x[0]
-			)
+    name = f"design_{panels[0]}_{panels[1]}_{panels[2]}_{inputs[0]},{inputs[1]}"
+    body = body_resolution(inputs[0],inputs[1],int(panels[0]),int(panels[1]),int(panels[2]))
+    body.name = name
+    nb_faces = body.mesh.nb_faces 
+    Amass , Bamping = get_hydro(body)
+    data = {'design':name,'panels':nb_faces,'A': float(Amass), 'B':float(Bamping)} 
+    return data
 
-			data = [{'design':bn,'panels':nb_faces,'lcoe':lcoe} for nb_faces,lcoe,bn in pairs ]
-			data = pd.DataFrame.from_dict(data)
-			df_list.append(data)
-			print("appendng dataframe")
-			df = pd.concat(df_list)
-			df.to_csv(f"data/{name}_{2}.csv")
-			iteration+=1
-	return iteration
+data = [by_design(inputs,panels) for inputs in inputs_dim for panels in inputs_panels]
+df = pd.DataFrame.from_dict(data)
+df.to_csv(f"../../data/mesh_convergence/convergence.csv")
 
-
-x= [by_design(inputs,panels) for inputs in inputs_dim for panels in inputs_panels]
-#fixed..deterministic
 
 
