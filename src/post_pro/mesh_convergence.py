c08c0708de99f2f911b1ec47ab0f1f83ec19fd0b
KapilKhanal <kkhanal16@winona.edu>
2024-03-06 20:21:32 -0500

    more mesh convergence for sample designs

--- src/post_pro/mesh_convergence.py
+++ src/post_pro/mesh_convergence.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from smt.sampling_methods import LHS
+import itertools
 from concurrent.futures import ProcessPoolExecutor
 import random, os , sys
 import capytaine as capy
+from plotnine import ggplot, aes, facet_wrap, labs, geom_line,scale_color_discrete
+import plotnine as pn
 random.seed(0)
 parent_folder = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 sys.path.insert(0,parent_folder)
@@ -47,41 +51,55 @@ def get_hydro(body):
 
 
 #Latin hypercube sampling to get representative design in the design space.
-radius,length_ratio = [1,10], [1,3]
+radius,length_ratio = [3,10], [5,20] # r':[2,10], 'L':[0.1,0.2] but L is diferrent here
 designs = np.array([radius, length_ratio])
 sampling = LHS(xlimits=designs)
-num = 100
+num = 9
 inputs_dim = sampling(num)
 
 df = pd.DataFrame(inputs_dim)
 
-nr = [8,20] #(nr) number of panels along a radius on the extremities of the cylinder
-ntheta = [10,30] #(ntheta) number of panels along a circular slice of the cylinder
-nx = [5,25] #number of circular slices(nx) ,
+nr = list(range(2,21,5))#(nr) number of panels along a radius on the extremities of the cylinder
+ntheta = list(range(20,40+1,5)) #(ntheta) number of panels along a circular slice of the cylinder
+nx = list(range(10,30+1,5)) #number of circular slices(nx) ,
+# # default for unit radius and length 10---> nx=10, ntheta=10, nr=2
 
-#Latin hypercube sampling to get all representative panels in the design space.
-panels = np.array([nr,nx,ntheta])
-sampling = LHS(xlimits=panels)
-num = 50
-inputs_panels= sampling(num)
-
+# Generate all possible combinations
+inputs_panels = list(itertools.product(nr, ntheta, nx))
 print(inputs_panels)
-
 # model.run after pack, unpack stuff...
 omega = 1.02
 
 def by_design(inputs,panels):
-    name = f"design_{panels[0]}_{panels[1]}_{panels[2]}_{inputs[0]},{inputs[1]}"
+    name = f"design_{inputs[0]},{inputs[1]}"
     body = body_resolution(inputs[0],inputs[1],int(panels[0]),int(panels[1]),int(panels[2]))
     body.name = name
+    body.resolution = (panels[0],panels[1],panels[2])
     nb_faces = body.mesh.nb_faces 
     Amass , Bamping = get_hydro(body)
-    data = {'design':name,'panels':nb_faces,'A': float(Amass), 'B':float(Bamping)} 
+    data = {'design':name,'nr':panels[0],'ntheta':panels[1],'nx':panels[2],'panels':nb_faces,'A': float(Amass), 'B':float(Bamping)} 
     return data
 
 data = [by_design(inputs,panels) for inputs in inputs_dim for panels in inputs_panels]
 df = pd.DataFrame.from_dict(data)
+
 df.to_csv(f"../../data/mesh_convergence/convergence.csv")
-
-
-
+grouped = df.groupby('design').ngroup()
+df['design_id'] = 'design' + grouped.astype(str)
+df['moving_average'] = df['A'].rolling(window=4, min_periods=4).mean()
+plt.figure(figsize=(14,10))
+x = ( ggplot(df)
+    + facet_wrap(facets="design_id",scales='free')
+    + aes(x="panels", y="A")
+    + labs(
+        x="panels",
+        y="A",
+        title="",
+    )
+    #+ geom_line(show_legend=True)+ pn.theme(panel_background=pn.element_blank()) 
+    + aes(y='moving_average') +  geom_line(color='red', linetype='dashed')
+ + pn.theme(axis_title_y=pn.element_blank())
+ + pn.theme(axis_ticks_major_y=pn.element_blank())
+ + pn.theme(figure_size=(12, 8))
+             ).draw()
+x.savefig("plots/mesh_convergence.pdf")
