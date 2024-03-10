6a13b32605aea25fe410639279e2ef5dfb7441f5
KapilKhanal <kkhanal16@winona.edu>
2024-03-10 14:45:04 -0400

    pull updates

--- src/post_pro/sensitivity.py
+++ src/post_pro/sensitivity.py
@@ -37,47 +37,51 @@ parameter_problem = {
 print(os.getcwd())
 
  #update this with optimal locations
-csv_file_path = os.path.join( '~/wec_array_opt/data/paretos', 'domDesignNewMesh.csv')
+csv_file_path = os.path.join( '~/wec_array_opt/data/paretos', 'FINALdomDesign.csv')
 df = pd.read_csv(csv_file_path, delimiter=',',header=None)
 
-# setting an index such that we get a few points along the Pareto front
-end = len(df.iloc[:,0])
 #index_range = np.arange(0, end, int(0.1 * end), dtype=int)
-index_range = np.arange(0, end, 1, dtype=int)
+index_range = np.arange(0, df.shape[0], 3, dtype=int)
 # for loop to calculate q-factor for Pareto optimal points
 some_pareto_designs = []
 
 for index in index_range:
     some_pareto_designs.append(df.iloc[index,:] )
 
+print(some_pareto_designs)
 #run theh 'nominal' values picked by sampler 
 def run_sensitivity_sampler(optimal_dv,N_samples,write_out = False):
     param_values = saltelli.sample(parameter_problem, N_samples)
     Y = np.empty([16*N_samples])
     for i, X in enumerate(param_values):
         p = [*X]
-        print(f'{p} | set number {i}')
         Y[i] = model.run(optimal_dv,p)[0] #one objective at a time
     
-        Si = sobol.analyze(parameter_problem, Y,calc_second_order=True, num_resamples=100, conf_level=0.95, print_to_console=False)
-        total_Si, first_Si, second_Si = Si.to_df()
+        Si = sobol.analyze(parameter_problem, Y,calc_second_order=True, num_resamples=1000, conf_level=0.95, print_to_console=False)
+    
+        total_Si,first_Si,second_Si = Si.to_df()
+        print(total_Si)
         if write_out:
-            total_Si.to_csv(f"../data/sensitivities/total_{i}.csv")
-            first_Si.to_csv(f"../data/sensitivities/first_{i}.csv")
-            second_Si.to_csv(f"../data/sensitivities/second_{i}.csv")
+            total_Si.to_csv(f"~/wec_array_opt/data/sensitivities/total.csv")
+            # first_Si.to_csv(f"~/wec_array_opt/data/sensitivities/first.csv")
+            # second_Si.to_csv(f"~/wec_array_opt/data/sensitivities/second.csv")
             print(f"wrote out the sensiitivity for design {i}..use plot_sensitivity.py to plot")
-        return np.mean(Y)
+        return total_Si
 
 #running sensitivity for one design to get idea number of samples to run.
-N = [2**i for i in np.range(0,10)] #,4000,5000,10000]
+N = [2**i for i in np.arange(5,14)] #,4000,5000,10000]
 #mean_Y = [run_sensitivity_sampler(some_pareto_designs[1],samples) for samples in N]
-
+print(N)
 # maybe run in parallel
 def run_parallel_sensitivity(N_values):
     with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
-        mean_Y = list(executor.map(lambda samples: run_sensitivity_sampler(some_pareto_designs[1], samples), N_values))
-    return mean_Y
+        total_SI = list(executor.map(lambda samples: run_sensitivity_sampler(some_pareto_designs[1], samples,write_out=True), N_values))
+    
+    for i, df in enumerate(total_SI):
+        df['samples'] = [N_values[i]] * len(df)
+    total_df = pd.concat(total_SI)
 
-mean_Y = run_parallel_sensitivity(N)
-
-np.savetxt("mean_Y_txt", mean_Y)
+    return total_df
+run_sensitivity_sampler(some_pareto_designs[0],1024,write_out = True)
+# total_df = run_parallel_sensitivity(N)
+# total_df.to_csv("~/wec_array_opt/data/sensitivities/total_SI_convergece.csv")
