6334375aa20f9cfb5edbd4927f33b93c48529902
degoeden <degoeden8@gmail.com>
2024-04-15 10:32:59 -0400

    5e5 f_max

--- src/main_moo_opt.py
+++ src/main_moo_opt.py
@@ -32,12 +32,12 @@ if __name__ == "__main__":
     #print(Xtable)
 
     X = [Xtable[i] for i in F1]
-    with open(f'../data/paretos/ReactObjective.csv', 'w', newline='') as csvfile:
+    with open(f'../data/paretos/ReactObjective_5.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         for i in range(len(F)):
             writer.writerow([F1[i],F2[i]]) # LCOE, max spacing
 
-    with open(f'../data/paretos/ReactDesign.csv', 'w', newline='') as csvfile:
+    with open(f'../data/paretos/ReactDesign_5.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         for i in range(len(X)):
             writer.writerow(X[i])
