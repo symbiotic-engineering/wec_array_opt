b0bf93de0061d8f8a2fe10523fd3e5d17ab497ec
degoeden <degoeden8@gmail.com>
2024-04-17 10:02:59 -0400

    fmax=1e5 pareto

--- src/post_pro/plot_pareto.py
+++ src/post_pro/plot_pareto.py
@@ -12,7 +12,7 @@ from scipy.interpolate import make_interp_spline
 import pyplotutilities.colors as colors
 
 f1, f2 = [], []
-with open('../data/paretos/ReactObjective_5.csv', newline='') as csvfile:
+with open('../data/paretos/ReactObjective_fmax1.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         f1.append(float(row[0]))
