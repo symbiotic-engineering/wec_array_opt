6408a1e43fca5db32d09b706d7c252b04a0d05d1
degoeden <degoeden8@gmail.com>
2024-04-25 11:57:40 -0400

    time data is false not zero now

--- src/modules/hydro_terms.py
+++ src/modules/hydro_terms.py
@@ -5,7 +5,7 @@ from capytaine.bem.airy_waves import froude_krylov_force
 import time
 # This module is used to get all the hydro terms. Forces, added mass, damping, hydrostatic stiffness, and mass
 
-def run(bodies,beta = 0,omega = 1.02,time_data = 0):
+def run(bodies,beta = 0,omega = 1.02,time_data = False):
     # bodies    ->  list of the wec floating bodies
     # beta      ->  wave direction
     # omega     ->  wave frequency
